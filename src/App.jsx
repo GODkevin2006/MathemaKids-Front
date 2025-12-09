4c830a4e2f7b17507ded5c75c96b2cfa8fa94339
Sharline171 <sharlinexiomara@gmail.com>
2025-12-09 19:57:06 +0100

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
+import CrearCuenta from './components/auth/CrearCuenta.jsx';
 import Login from './components/auth/Login';
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
+
         <Routes>
+          {/* Ruta del Login */}
           <Route path="/login" element={<Login />} />
+
+          {/* Ruta de Crear Cuenta */}
+          <Route path="/crearcuenta" element={<CrearCuenta />} />
         </Routes>
+
       </AuthProvider>
     </BrowserRouter>
   );
